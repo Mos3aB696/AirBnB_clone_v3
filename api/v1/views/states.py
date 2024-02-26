13361dedeb7ebf9198bba46167bc451dde9aa5b0
Mos3aB696 <mos3ab696@gmail.com>
2024-02-26 23:36:31 +0200

    Ta_6_sk

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -12,7 +12,7 @@ from api.v1.views import app_views
 from models.state import State
 
 
-@app_views.route('/states', methods=['GET'], strict_slashes=False)
+@app_views.route('/states', methods=["GET"], strict_slashes=False)
 def get_states():
     """
     Retrieve the list of all State objects.
@@ -25,7 +25,7 @@ def get_states():
     return jsonify(state_list)
 
 
-@app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
+@app_views.route('/states/<state_id>', methods=["GET"], strict_slashes=False)
 def get_state(state_id):
     """
     Retrieve a State object.
@@ -36,7 +36,7 @@ def get_state(state_id):
     return jsonify(state.to_dict())
 
 
-@app_views.route('/states/<state_id>', methods=['DELETE'],
+@app_views.route('/states/<state_id>', methods=["DELETE"],
                  strict_slashes=False)
 def delete_state(state_id):
     """
@@ -50,7 +50,7 @@ def delete_state(state_id):
     return jsonify({})
 
 
-@app_views.route('/states', methods=['POST'], strict_slashes=False)
+@app_views.route('/states', methods=["POST"], strict_slashes=False)
 def post_state():
     """
     Create a State object.
@@ -67,7 +67,7 @@ def post_state():
     return make_response(jsonify(state.to_dict()), 201)
 
 
-@app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
+@app_views.route('/states/<state_id>', methods=["PUT"], strict_slashes=False)
 def put_state(state_id):
     """
     Update a State object.
@@ -78,7 +78,7 @@ def put_state(state_id):
     if not request.json:
         abort(400, 'Not a JSON')
     for key, value in request.get_json().items():
-        if key not in ['id', 'created_at', 'updated_at']:
+        if key not in ["id", "created_at", "updated_at"]:
             setattr(state, key, value)
     state.save()
     return jsonify(state.to_dict())
