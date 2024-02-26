8b8bce870154bdf6cf35ff8548f7c5f73d6f239c
Amirametwally <amira.metwally56@gmail.com>
2024-02-27 01:58:35 +0200

    Search

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -9,6 +9,8 @@ from models import storage
 from models.city import City
 from models.place import Place
 from models.user import User
+from models.state import State
+from models.amenity import Amenity
 
 
 @app_views.route("cities/<city_id>/places", methods=["GET"],
@@ -94,3 +96,36 @@ def modify_place(place_id):
             setattr(place, key, value)
     place.save()
     return make_response(jsonify(place.to_dict()), 200)
+
+
+@app_views.route("/places_search", methods=["POST"], strict_slashes=False)
+def places_search():
+    """
+    Search places based on optional filters: states, cities, amenities.
+    """
+    dictionary = request.get_json()
+    if dictionary is None:
+        abort(400, "Not a JSON")
+
+    states = dictionary.get("states", [])
+    cities = dictionary.get("cities", [])
+    amenities = dictionary.get("amenities", [])
+
+    if not states and not cities and not amenities:
+        places = storage.all(Place).values()
+    else:
+        places = []
+        for place in storage.all(Place).values():
+            if states and place.city.state.id not in states:
+                continue
+            if cities and place.city.id not in cities:
+                continue
+            places.append(place)
+
+        if amenities:
+            places = [place for place in places
+                      if all(amenity.id in place.amenities
+                             for amenity in amenities)]
+
+    places_dict = [place.to_dict() for place in places]
+    return jsonify(places_dict)
