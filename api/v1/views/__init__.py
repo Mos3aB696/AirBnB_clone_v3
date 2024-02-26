ca86cf000361b35ae134805378c6db8d04374caf
Amirametwally <amira.metwally56@gmail.com>
2024-02-26 15:54:51 +0200

    initialization

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -6,6 +6,7 @@
   `api/v1/views` directory.
 """
 from flask import Blueprint
-from api.v1.views.index import *
-from cities import *
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+from api.v1.views.index import *
+from .cities import *
