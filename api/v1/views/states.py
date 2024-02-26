d08e6b0568c28610183a82155aff47b99c934fdd
Mos3aB696 <mos3ab696@gmail.com>
2024-02-27 00:02:16 +0200

    Ta_6_sk

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,34 +1,39 @@
 #!/usr/bin/python3
 """
-    routes for CRUD operations on State objects in a Flask application.
-    :return: The code provided is a Flask application with
-    routes for handling CRUD operations on State
-    objects. The functions defined in the code include:
+New view for State objects that handles all default RESTFul API actions.
 """
 
-from flask import jsonify, abort, make_response, request
+from flask import jsonify, abort, request, make_response
 from models import storage
-from api.v1.views import app_views
 from models.state import State
+from api.v1.views import app_views
 
 
-@app_views.route('/states', methods=["GET"], strict_slashes=False)
+@app_views.route("/states", methods=["GET"], strict_slashes=False)
 def get_states():
     """
     Retrieve the list of all State objects.
 
+    Returns:
+        JSON response: A JSON response containing a list of all State objects.
     """
     states = storage.all(State)
-    state_list = []
+    list_states = []
     for state in states.values():
-        state_list.append(state.to_dict())
-    return jsonify(state_list)
+        list_states.append(state.to_dict())
+    return jsonify(list_states)
 
 
-@app_views.route('/states/<state_id>', methods=["GET"], strict_slashes=False)
+@app_views.route("/states/<state_id>", methods=["GET"], strict_slashes=False)
 def get_state(state_id):
     """
     Retrieve a State object.
+
+    Args:
+        state_id (str): The UUID4 string representing a State object.
+
+    Returns:
+        JSON response: A JSON response containing a State object.
     """
     state = storage.get(State, state_id)
     if state is None:
@@ -36,11 +41,17 @@ def get_state(state_id):
     return jsonify(state.to_dict())
 
 
-@app_views.route('/states/<state_id>', methods=["DELETE"],
+@app_views.route("/states/<state_id>", methods=["DELETE"],
                  strict_slashes=False)
 def delete_state(state_id):
     """
     Delete a State object.
+
+    Args:
+        state_id (str): The UUID4 string representing a State object.
+
+    Returns:
+        JSON response: An empty JSON response.
     """
     state = storage.get(State, state_id)
     if state is None:
@@ -50,7 +61,7 @@ def delete_state(state_id):
     return jsonify({})
 
 
-@app_views.route('/states', methods=["POST"], strict_slashes=False)
+@app_views.route("/states", methods=["POST"], strict_slashes=False)
 def post_state():
     """
     Create a State object.
@@ -59,24 +70,30 @@ def post_state():
         JSON response: A JSON response containing a new State object.
     """
     if not request.json:
-        abort(400, 'Not a JSON')
-    if 'name' not in request.json:
-        abort(400, 'Missing name')
+        abort(400, "Not a JSON")
+    if "name" not in request.json:
+        abort(400, "Missing name")
     state = State(**request.get_json())
     state.save()
     return make_response(jsonify(state.to_dict()), 201)
 
 
-@app_views.route('/states/<state_id>', methods=["PUT"], strict_slashes=False)
+@app_views.route("/states/<state_id>", methods=["PUT"], strict_slashes=False)
 def put_state(state_id):
     """
     Update a State object.
+
+    Args:
+        state_id (str): The UUID4 string representing a State object.
+
+    Returns:
+        JSON response: A JSON response containing an updated State object.
     """
     state = storage.get(State, state_id)
     if state is None:
         abort(404)
     if not request.json:
-        abort(400, 'Not a JSON')
+        abort(400, "Not a JSON")
     for key, value in request.get_json().items():
         if key not in ["id", "created_at", "updated_at"]:
             setattr(state, key, value)
@@ -84,5 +101,5 @@ def put_state(state_id):
     return jsonify(state.to_dict())
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     pass
